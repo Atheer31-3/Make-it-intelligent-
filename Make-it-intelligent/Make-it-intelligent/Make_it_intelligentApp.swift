423e9fb8878d05b985036062abe710ca9be6f6b4
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2025-02-26 23:41:37 +0300

    create foders model, viewmodel, screen

---
+++ Make-it-intelligent/Make-it-intelligent/Make_it_intelligentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_it_intelligentApp.swift
+//  Make-it-intelligent
+//
+//  Created by atheer alshareef on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Make_it_intelligentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

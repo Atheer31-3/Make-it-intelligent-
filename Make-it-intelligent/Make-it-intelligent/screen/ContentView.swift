aef0e983d1f59fb651883ef4f3bb5a7bf48ed984
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2025-03-02 11:51:45 +0300

    add screen

---
+++ Make-it-intelligent/Make-it-intelligent/screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Make-it-intelligent
+//
+//  Created by atheer alshareef on 26/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
